dd05a16d13030b0e20a81d8e3236eb498a07f01c
swapnilpatil <swapnilpatil242@gmail.com>
2019-09-29 15:58:44 +0530

    Added author for article model
    - Added customize class name from application/config/blorgh
    - Added authur_id migration into article
    - Updated view with author_id
    - Updated model configuration and association

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_21_034250) do
+ActiveRecord::Schema.define(version: 2019_09_29_102035) do
 
   create_table "blorgh_articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "author_id"
   end
 
 end
